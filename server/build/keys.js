3267223e19f21473c3b25366677f65176e4b5424
Eduardo Favela <45271973+eduardo-favela@users.noreply.github.com>
2022-08-03 16:58:11 -0500

    update

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: '10.1.11.47',
-        user: 'admin',
-        password: '12345',
-        database: 'ssd',
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'sistematicketspag',
         connectionLimit: 30
     }
 };
